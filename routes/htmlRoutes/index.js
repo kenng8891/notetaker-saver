22dfe3112b6e1c9d07d9e9f637af1182b4357090
kenng8891 <103011617+kenng8891@users.noreply.github.com>
2022-06-28 01:09:39 -0400

    html

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../develop/public/index.html'))
-});
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../develop/public/notes.html'))
 })
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../develop/public/index.html'))
+});
+
 
 module.exports = router;
